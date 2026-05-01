6ef8238f9e63e97de257ce08a29d51908523211d
shlomi <shlomi.shakir@gmail.com>
2026-05-01 18:46:28 +0300

    rm comments

--- backend/controller/pdf_controller.py
+++ backend/controller/pdf_controller.py
@@ -18,8 +18,3 @@ def index_uploaded_pdf(background_tasks: BackgroundTasks, file: UploadFile= File
         background_tasks.add_task(rag_service.index_chunks_in_vector_db, chunks=pdf_res.chunks, file_name=file.filename)
         # Step 3: Return PDFResponse immediately
     return pdf_res
-
-
-# TODO 1: allow the user to update a new file that overrides the old he uploaded before, we need to replace the store at the front into db instead- infra change
-# TODO 2 : create a pannel that shows thumbnail of the file that was uploaded at the left hand side
-# TODO 3 : ask if the files under unchanged should be commited and make a stash to checkout to a feature branch since we are on main
