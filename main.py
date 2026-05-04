a79b25b2651cd0f8cacdf8a595ac21de373cb6cf
shlomi <shlomi.shakir@gmail.com>
2026-05-04 17:44:02 +0300

    replaced vercel path

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3001"],
-    allow_methods=["https://pdf-rag-demo.vercel.app"],
+    allow_origins=[
+        "http://localhost:3001",
+        "https://pdf-rag-demo.vercel.app",
+    ],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
