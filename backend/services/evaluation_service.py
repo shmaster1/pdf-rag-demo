806fff5416c06f678fcdcba241767f5995a84eea
shlomi <shlomi.shakir@gmail.com>
2026-05-15 17:43:33 +0300

    moving imports

--- backend/services/evaluation_service.py
+++ backend/services/evaluation_service.py
@@ -1,16 +1,15 @@
 from typing import List
-import ragas
-from datasets  import Dataset
-from ragas.metrics import answer_relevancy, faithfulness
-from ragas.llms import LangchainLLMWrapper
-from ragas.embeddings import LangchainEmbeddingsWrapper
-from langchain_huggingface import HuggingFaceEndpoint, HuggingFaceEmbeddings
 from backend.config.config import Config
 
 
 class EvaluationService:
 
     def __init__(self):
+        import ragas  # noqa: F401
+        from ragas.llms import LangchainLLMWrapper
+        from ragas.embeddings import LangchainEmbeddingsWrapper
+        from langchain_huggingface import HuggingFaceEndpoint, HuggingFaceEmbeddings
+
         self.config = Config()
         self.hf_llm = HuggingFaceEndpoint(model="Qwen/Qwen2.5-7B-Instruct", huggingfacehub_api_token=self.config.HUGGING_FACE_KEY)
         self.langchain_llm_wrapper = LangchainLLMWrapper(self.hf_llm)
